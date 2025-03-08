4f7e11d700cda153d0dc8d2554f1ee393478ada2
SaTh <Sathsara2021@gmail.com>
2025-03-09 02:28:37 +0530

    state-machine(idle/walk)

--- Player/Scripts/player.gd
+++ Player/Scripts/player.gd
@@ -2,27 +2,21 @@ class_name Player extends CharacterBody2D
 
 var cardinal_direction: Vector2 = Vector2.DOWN
 var direction: Vector2 = Vector2.ZERO
-var move_speed: float = 100.0
-var state: String = "idle"
 
 @onready var animation_player: AnimationPlayer = $AnimationPlayer
 @onready var sprite: Sprite2D = $Sprite2D
+@onready var state_machine: PlayerStateMachine = $StateMachine
 
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	UpdateAnimation()
+	state_machine.Initialize(self)
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	direction.x = Input.get_action_strength("right") - Input.get_action_strength("left")
 	direction.y = Input.get_action_strength("down") - Input.get_action_strength("up")
-	
-	velocity = direction * move_speed
-	
-	if SetState() || SetDirection():
-		UpdateAnimation()
 
 
 func _physics_process(delta: float) -> void:
@@ -45,19 +39,9 @@ func SetDirection() -> bool:
 	cardinal_direction = new_direction
 	sprite.scale.x = -1 if cardinal_direction == Vector2.LEFT else 1
 	return true
-	
-
-func SetState() -> bool:
-	var new_state: String = "idle" if direction == Vector2.ZERO else "walk"
-	
-	if new_state == state:
-		return false
-	
-	state = new_state
-	return true
 
 
-func UpdateAnimation() -> void:
+func UpdateAnimation(state: String) -> void:
 	var anim_direction: String = AnimDirection()
 	animation_player.play(state + "_" + anim_direction)
 
